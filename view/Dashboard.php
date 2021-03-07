70e47d85a5f114c4151efd1df0dd36e5f38ab4b7
HabiburRahmanAnik <habib264521@gmail.com>
2021-03-07 22:37:24 +0600

    add dashboard.php

---
+++ view/Dashboard.php
@@ -0,0 +1,39 @@
+<?php
+	
+	if (!isset($_COOKIE['status'])) {
+		header('location: login.html');
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" href="../assets/002-planting.png"><title>Dashboard</title>
+</head>
+<body>
+
+	<table border="1" align="center" width="100%">
+ 		<tr height="100px">
+ 			<td width="100px"><img src="../assets/002-planting.png" width="150px" height="70px"></td>
+ 			<td align="right">
+ 				Logged in as <?php echo $_COOKIE['username']?> | 
+ 				<a href="../controller/logout.php">Logout</a>
+ 			</td>
+	 	</tr>
+	 	<tr height="400px">
+	 		<td width="300px">
+	 			<ul>
+	 				<?php include('link.php'); ?>
+	 			</ul>
+	 		</td>
+	 		<td><h3>Welcome Admin Panel</h3></td>
+	 	</tr>
+		<tr height="50px">
+			<td colspan="3" align="center">
+				copyright@2021
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
