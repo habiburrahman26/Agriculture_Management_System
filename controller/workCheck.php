c5c761bd3d2a322f4d5e3ea38d21cddbe1d60670
HabiburRahmanAnik <habib264521@gmail.com>
2021-04-26 20:55:26 +0600

    modify

--- controller/workCheck.php
+++ controller/workCheck.php
@@ -1,38 +1,38 @@
 <?php
-	session_start();
-	require_once('../model/workModel.php');
+session_start();
+require_once '../model/workModel.php';
 
-	if(isset($_POST['submit'])){
+if ( isset( $_POST['submit'] ) ) {
 
-		$username 	= $_POST['username'];
-		$task 		= $_POST['task'];
-		$email	 	= $_POST['email'];
-		$period 	= $_POST['period'];
-		$location 	= $_POST['location'];
-		$salery	 	= $_POST['salery'];
+    $username = $_POST['username'];
+    $task = $_POST['task'];
+    $email = $_POST['email'];
+    $period = $_POST['period'];
+    $location = $_POST['location'];
+    $salery = $_POST['salery'];
 
-		if($username == "" || $task == "" || $email == "" || $period == "" ||  $location == "" || $salery == "" ){
-			echo "Invalid information.	Please try again!";
-		}else{
-		
-				$user = [
-							'username'	=>$username, 
-							'task'		=>$task, 
-							'email'		=>$email,
-							'period'	=>$period, 
-							'location'	=>$location, 
-							'salery'	=>$salery
-						];
+    if ( $username == "" || $task == "" || $email == "" || $period == "" || $location == "" || $salery == "" ) {
+        echo "Invalid information.	Please try again!";
+    } else {
 
-				$status = insertWork($user);
+        $user = [
+            'username' => $username,
+            'task'     => $task,
+            'email'    => $email,
+            'period'   => $period,
+            'location' => $location,
+            'salery'   => $salery,
+        ];
 
-				if($status){
-					header('location: ../view/workList.php');
-				}else{
-					echo "Something Error";
-				}
-		}
-	}else{
-		echo	"Sorry";
-	}
+        $status = insertWork( $user );
+
+        if ( $status ) {
+            header( 'location: ../view/workList.php' );
+        } else {
+            echo "Something Error";
+        }
+    }
+} else {
+    echo "Sorry";
+}
 ?>
